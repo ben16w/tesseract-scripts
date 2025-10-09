14ced1a4e8a5b9ee847228fec6447b2fd9a4574e
ben <benjaminwadsworth@gmail.com>
2025-10-09 10:44:37 +0000

    refactor: move common functions to shared.sh for reuse

--- mirror_backup.sh
+++ mirror_backup.sh
@@ -18,66 +18,8 @@ function usage()
     exit 0
 }
 
-####  COMMON CODE  ####
-
-LOG_FILE=""
-EMAIL_USERNAME=""
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-# Function to exit the script with an error message.
-function error_exit()
-{
-    echo "ERROR: ${1:-"Unknown Error"}"
-    log "ERROR: ${1:-"Unknown Error"}"
-
-    if [ "${EMAIL_USERNAME}" != "" ]; then
-
-msmtp -t <<EOF
-To: ${EMAIL_USERNAME}
-From: ${EMAIL_USERNAME}
-Subject: $(hostname): Script $0 has encountered an error - ${1:-"Unknown Error"}
-
-Hostname: $(hostname)
-Logs:
-$(tail -n 10 "$LOG_FILE")
-EOF
-
-    else
-        info "No email sent. EMAIL_USERNAME not set."
-    fi
-    exit 1
-}
-
-# Function to print an informational message.
-function info()
-{
-    echo "INFO: ${1}"
-    log "INFO: ${1}"
-}
-
-function log()
-{
-    if [ "${LOG_FILE}" != "" ]; then
-        echo "$(date '+%F %T.%3N') ${1}" >> "$LOG_FILE"
-    fi
-}
-
-# Source environment variables from the .env file if it exists
-if [ -f "$SCRIPT_DIR"/.env ]; then
-    echo "Sourcing environment variables from $SCRIPT_DIR/.env"
-    source "$SCRIPT_DIR"/.env || exit 1
-fi
-
-# Check if script is running as root
-if [[ $EUID -ne 0 ]]; then
-    error_exit "Script $0 must be run as root" 
-fi
-
-# Display usage information if -h or --help option is provided
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    usage
-fi
-
+source "$SCRIPT_DIR/shared.sh"
 
 ####  MAIN CODE  ####
 
